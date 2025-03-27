6e67530529e69fab036476279f715e89977440ab
Chirag <chiragd@playsimple.in>
2025-03-28 03:14:22 +0530

    #folder structure changed #imports changed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from grid_gen.grid_gen_bitmaps import ccw_csp
-from content_mapping_gen import content_mapping_ui
+from ui.grid_generator.ccw_csp import main as ccw_csp_main
+from ui.content_mapping.content_mapping_ui import main as content_mapping_ui_main
 
 def main():
     st.title("Crossword Go Tool")
@@ -13,10 +13,10 @@ def main():
     )
 
     if selected_option == "Generate Grids":
-        ccw_csp.main()
+        ccw_csp_main()
         
     elif selected_option == "Generate Content Mapping":
-        content_mapping_ui.main()
+        content_mapping_ui_main()
         # Content mapping logic will go here
 
 if __name__ == "__main__":
